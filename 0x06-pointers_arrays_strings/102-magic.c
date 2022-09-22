0b810275d218e67f5d30c3021737928d9b02f155
root <you@example.com>
2022-09-22 12:58:04 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
 
 int main(void)
@@ -21,28 +20,4 @@ int main(void)
   /* ...so that this prints 98\n */
   printf("a[2] = %d\n", a[2]);
   return (0);
-=======
-#include <stdio.h>                                                                                           
-                                                                                                             
-int main(void)                                                                                               
-{                                                                                                            
-        int n;                                                                                               
-        int a[5];                                                                                            
-        int *p;                                                                                              
-                                                                                                             
-        a[2] = 1024;                                                                                         
-        p = &n;                                                                                              
-        /*                                                                                                   
-         * write your line of code here...                                                                   
-         * Remember:                                                                                         
-         * - you are not allowed to use a                                                                    
-         * - you are not allowed to modify p                                                                 
-         * - only one statement                                                                              
-         * - you are not allowed to code anything else than this line of code                                
-         */                                                                                                  
-        *(p + 5) = 98;                                                                                
-        /* ...so that this prints 98\n */                                                                    
-        printf("a[2] = %d\n", a[2]);                                                                         
-        return (0);                                                                                          
->>>>>>> 2d2134808f71262f71cb38b9fc947310217957da
 }
